9c6bdd537782f6acc7e2b87e170117d418a6153f
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-19 23:33:27 -0500

    Update 15_Cards_Issued_Difference.sql

--- Solutions/Easy/15_Cards_Issued_Difference.sql
+++ Solutions/Easy/15_Cards_Issued_Difference.sql
@@ -2,3 +2,8 @@ WITH cte AS (SELECT
 card_name, MAX(issued_amount) AS highest, MIN(issued_amount) AS lowest
 FROM monthly_cards_issued
 GROUP BY 1)
+
+SELECT
+card_name, highest-lowest AS difference
+FROM cte
+ORDER BY 2 DESC;
