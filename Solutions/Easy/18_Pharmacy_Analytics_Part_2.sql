b5738b268416f7d23506ee6dc76fb57efacfdb46
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-20 10:02:23 -0500

    Update 18_Pharmacy_Analytics_Part_2.sql

--- Solutions/Easy/18_Pharmacy_Analytics_Part_2.sql
+++ Solutions/Easy/18_Pharmacy_Analytics_Part_2.sql
@@ -1,1 +1,10 @@
+WITH cte AS (SELECT
+manufacturer, drug, total_sales - cogs AS total_loss
+FROM pharmacy_sales)
 
+SELECT
+manufacturer, COUNT(drug) AS drug_count, ABS(SUM(total_loss)) AS total_loss
+FROM cte 
+WHERE total_loss <0
+GROUP BY 1
+ORDER BY 3 DESC;
