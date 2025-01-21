ac24abd1dfc27ef60e929ab2fab8d57d338eb4d5
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-21 12:28:03 -0500

    Update 01_User's_Third_Transaction.sql

--- Solutions/Medium/01_User's_Third_Transaction.sql
+++ Solutions/Medium/01_User's_Third_Transaction.sql
@@ -1,1 +1,6 @@
+WITH cte AS (SELECT
+*, ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS num
+FROM transactions)
 
+SELECT user_id, spend, transaction_date
+FROM cte WHERE num = 3;
