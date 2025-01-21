a28aed2b94fc3e55f15bde9b61f698471c4d869c
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-21 12:28:47 -0500

    Update 04_Tweets'_Rolling_Averages.sql

--- Solutions/Medium/04_Tweets'_Rolling_Averages.sql
+++ Solutions/Medium/04_Tweets'_Rolling_Averages.sql
@@ -1,1 +1,9 @@
+WITH cte AS (SELECT 
+user_id, tweet_date, 
+AVG(tweet_count) OVER(PARTITION BY user_id ORDER BY tweet_date ASC ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) 
+AS rolling_avg_3rd
+FROM tweets
+GROUP BY 1,2, tweet_count)
 
+SELECT user_id, tweet_date, ROUND(rolling_avg_3rd,2) AS rolling_avg_3rd
+FROM cte;
