44a24a620a27ef04f3e532750d68126a77c1389a
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-22 10:46:13 -0500

    Update 05_HIghest_Grossing_Items.sql

--- Solutions/Medium/05_HIghest_Grossing_Items.sql
+++ Solutions/Medium/05_HIghest_Grossing_Items.sql
@@ -1,1 +1,6 @@
-
+WITH cte AS (SELECT
+category, product, COUNT(product), SUM(spend) AS total_spend, DENSE_RANK() OVER(PARTITION BY category ORDER BY SUM(spend) DESC) AS rnk
+FROM product_spend
+WHERE EXTRACT(YEAR FROM transaction_date)::INT = 2022
+GROUP BY 1,2)
+SELECT category, product, total_spend FROM cte WHERE rnk <3
