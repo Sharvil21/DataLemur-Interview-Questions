c20db092755ea232862b6f1c05c781b1247cabe2
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-22 10:46:59 -0500

    Update 07_Top_5_Artists.sql

--- Solutions/Medium/07_Top_5_Artists.sql
+++ Solutions/Medium/07_Top_5_Artists.sql
@@ -1,1 +1,8 @@
-
+WITH cte AS (SELECT artist_name, DENSE_RANK() OVER(ORDER BY COUNT(artist_name) DESC) AS artist_rank
+FROM artists
+JOIN songs USING(artist_id)
+JOIN global_song_rank USING(song_id)
+WHERE rank < 11
+GROUP BY artist_name)
+SELECT * FROM cte
+WHERE artist_rank <6
