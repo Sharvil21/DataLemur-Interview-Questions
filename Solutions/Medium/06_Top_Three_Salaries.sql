8d0045ade11112693493ff0a43db631cd2b39579
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-22 10:46:36 -0500

    Update 06_Top_Three_Salaries.sql

--- Solutions/Medium/06_Top_Three_Salaries.sql
+++ Solutions/Medium/06_Top_Three_Salaries.sql
@@ -1,1 +1,9 @@
-
+WITH cte AS (SELECT name, department_name, salary, DENSE_RANK() OVER(PARTITION BY department_name ORDER BY salary DESC) AS rnk
+FROM 
+employee
+JOIN department USING(department_id))
+SELECT
+department_name, name, salary
+FROM cte
+WHERE rnk <4
+ORDER BY department_name ASC, salary DESC;
