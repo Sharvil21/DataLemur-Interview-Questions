ff95e39cceb11e82994719fc7d5bd76375fd76a7
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-22 10:47:32 -0500

    Update 08_Signup_Activation_Rate.sql

--- Solutions/Medium/08_Signup_Activation_Rate.sql
+++ Solutions/Medium/08_Signup_Activation_Rate.sql
@@ -1,1 +1,9 @@
+WITH cte AS (SELECT
+SUM(CASE WHEN signup_action = 'Confirmed' THEN 1 ELSE 0 END) AS confirmed,
+COUNT(*) AS total
+FROM emails
+LEFT JOIN texts USING(email_id)
+WHERE signup_action IS NOT NULL)
 
+SELECT
+ROUND(1.0*confirmed/total,2) FROM cte;
