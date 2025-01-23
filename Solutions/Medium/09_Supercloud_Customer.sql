1746606c5b31d62ecbc8608664327c516ccd8e74
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-23 11:24:50 -0500

    Update 09_Supercloud_Customer.sql

--- Solutions/Medium/09_Supercloud_Customer.sql
+++ Solutions/Medium/09_Supercloud_Customer.sql
@@ -1,1 +1,8 @@
+WITH cte AS (SELECT
+customer_id, COUNT(DISTINCT product_category) AS total_purchased
+FROM customer_contracts
+JOIN products USING(product_id)
+GROUP BY 1)
 
+SELECT customer_id FROM cte
+WHERE total_purchased = (SELECT COUNT(DISTINCT product_category) FROM products)
