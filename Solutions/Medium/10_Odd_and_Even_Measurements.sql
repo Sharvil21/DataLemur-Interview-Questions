4adb413c1b4a66fe381f6a09a9d2d3599aa5e99d
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-23 11:25:09 -0500

    Update 10_Odd_and_Even_Measurements.sql

--- Solutions/Medium/10_Odd_and_Even_Measurements.sql
+++ Solutions/Medium/10_Odd_and_Even_Measurements.sql
@@ -1,1 +1,10 @@
+WITH cte AS (SELECT
+*, DATE(measurement_time) AS measurement_day, ROW_NUMBER() OVER(PARTITION BY DATE(measurement_time) ORDER BY measurement_id ASC) AS rownum
+FROM measurements)
 
+SELECT 
+measurement_day,
+SUM(CASE WHEN rownum%2 =1 THEN measurement_value ELSE 0 END) AS odd_sum,
+SUM(CASE WHEN rownum%2 =0 THEN measurement_value ELSE 0 END) AS even_sum 
+FROM cte
+GROUP BY 1
