ebfec75da8fb73e17faeedcd12640ff44033cb51
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-23 11:25:26 -0500

    Update 11_Swapped_Food_Delivery.sql

--- Solutions/Medium/11_Swapped_Food_Delivery.sql
+++ Solutions/Medium/11_Swapped_Food_Delivery.sql
@@ -1,1 +1,14 @@
+WITH cte AS (SELECT
+*, COUNT(*) OVER() AS total_orders
+FROM
+orders)
 
+SELECT
+CASE
+  WHEN order_id = total_orders THEN order_id
+  WHEN order_id%2 = 0 THEN order_id - 1
+  WHEN order_id%2 <> 0 THEN order_id + 1
+  END AS corrected_order_id,
+  item
+FROM cte
+ORDER BY 1 ASC;
