e01742e1d8956a76f56e2229ddfc00daf58bd391
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-23 11:25:42 -0500

    Update 12_FAANG_Stock_Min-Max_Part_1.sql

--- Solutions/Medium/12_FAANG_Stock_Min-Max_Part_1.sql
+++ Solutions/Medium/12_FAANG_Stock_Min-Max_Part_1.sql
@@ -1,1 +1,13 @@
+WITH cte1 AS (SELECT
+ticker, open, to_char(date, 'Mon-YYYY') AS highest_mth, DENSE_RANK() OVER(PARTITION BY ticker ORDER BY open DESC) AS rnk1
+FROM stock_prices),
+cte2 AS (
+SELECT
+ticker, open, to_char(date, 'Mon-YYYY') AS lowest_mth, DENSE_RANK() OVER(PARTITION BY ticker ORDER BY open ASC) AS rnk2
+FROM stock_prices)
 
+SELECT 
+ticker, highest_mth, cte1.open AS highest_open,
+lowest_mth, cte2.open AS lowest_open
+FROM cte1 JOIN cte2 USING(ticker)
+WHERE rnk1 = 1 AND rnk2 = 1
