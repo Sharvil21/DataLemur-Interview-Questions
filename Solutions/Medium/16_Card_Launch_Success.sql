32a305f0fa59d5cacf1c0f9318ff9ac7735d26fd
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-24 11:46:51 -0500

    Update 16_Card_Launch_Success.sql

--- Solutions/Medium/16_Card_Launch_Success.sql
+++ Solutions/Medium/16_Card_Launch_Success.sql
@@ -1,1 +1,7 @@
+WITH cte AS (SELECT
+*, ROW_NUMBER() OVER(PARTITION BY card_name ORDER BY issue_year,issue_month) AS row_num
+FROM monthly_cards_issued)
 
+SELECT card_name, issued_amount 
+FROM cte WHERE row_num = 1
+ORDER BY 2 DESC;
