36618cad4e4779219ff15d7b4661b305d16c0550
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-24 11:46:19 -0500

    Update 14_Histogram_of_Users_and_Purchases.sql

--- Solutions/Medium/14_Histogram_of_Users_and_Purchases.sql
+++ Solutions/Medium/14_Histogram_of_Users_and_Purchases.sql
@@ -1,1 +1,8 @@
+WITH cte AS (SELECT
+transaction_date, user_id, COUNT(user_id) AS purchase_count, DENSE_RANK() OVER(PARTITION BY user_id ORDER BY transaction_date DESC) AS rnk
+FROM user_transactions
+GROUP BY user_id, transaction_date)
 
+SELECT transaction_date, user_id, purchase_count
+FROM cte WHERE rnk = 1
+ORDER BY 1;
