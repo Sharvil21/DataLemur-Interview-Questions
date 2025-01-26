53cb1d8b3c2ac148a7de454579152b6abe4a5650
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-25 22:06:22 -0500

    Update 02_Y-on-Y_Growth_Rate.sql

--- Solutions/Hard/02_Y-on-Y_Growth_Rate.sql
+++ Solutions/Hard/02_Y-on-Y_Growth_Rate.sql
@@ -1,1 +1,11 @@
+WITH cte AS (SELECT
+EXTRACT(YEAR FROM transaction_date) AS year,
+product_id, spend, LAG(spend) OVER(PARTITION BY product_id ORDER BY EXTRACT(YEAR FROM transaction_date) ASC) AS previous_year_spend
+FROM user_transactions
+ORDER BY product_id, 1)
 
+SELECT
+year, product_id,
+spend AS current_year_spend, previous_year_spend AS prev_year_spend,
+ROUND((100.0*(spend - previous_year_spend)/previous_year_spend),2) AS yoy_rate
+FROM cte;
