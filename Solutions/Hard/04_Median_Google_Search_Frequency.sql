82aaf0c83d1ebef3250ca47cc2aa1aedaf3e892c
Sharvil Shastri <46630953+Sharvil21@users.noreply.github.com>
2025-01-27 13:57:11 -0500

    Update 04_Median_Google_Search_Frequency.sql

--- Solutions/Hard/04_Median_Google_Search_Frequency.sql
+++ Solutions/Hard/04_Median_Google_Search_Frequency.sql
@@ -1,6 +1,18 @@
+--Method 1
+
 WITH cte AS (SELECT
 searches, GENERATE_SERIES(1,num_users)
 FROM search_frequency)
 
 SELECT PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY searches ASC) AS median
 FROM cte;
+
+
+--Similar Method 2
+WITH cte AS (SELECT
+searches, GENERATE_SERIES(1,num_users) AS user_number
+FROM search_frequency)
+
+SELECT
+1.0*PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY searches ASC) AS median
+FROM cte
