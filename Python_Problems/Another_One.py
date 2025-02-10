5c969477a5910629e1efde4f4e8d6ad7e5467c1d
Sharvil21 <sshastri2102@gmail.com>
2025-02-10 13:17:32 -0500

    Added the solution

---
+++ Python_Problems/Another_One.py
@@ -0,0 +1,14 @@
+def another_one(digits):
+  
+  i = len(digits)-1
+  
+  while i >= 0 and digits[i] == 9:
+    digits[i] = 0
+    i = i-1
+  
+  if i >=0:
+    digits[i] += 1
+    return digits
+  
+  else:
+    return [1] + digits
